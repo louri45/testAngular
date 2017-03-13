4746e845ef0d34c7520b6f0e8152d9f0ec5a0b7a
louri45 <l.richard45@gmail.com>
2017-03-13 17:26:52 +0100

    FIN Rooting

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
+
 @Injectable()
 export class HeroService {
   getHeroes(): Promise<Hero[]> {
@@ -8,9 +9,14 @@ export class HeroService {
   }
 
   getHeroesSlowly(): Promise<Hero[]> {
-  return new Promise(resolve => {
-    // Simulate server latency with 2 second delay
-    setTimeout(() => resolve(this.getHeroes()), 2000);
-  });
-}
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    });
+  }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+               .then(heroes => heroes.find(hero => hero.id === id));
+  }
 }
